420536f5374dd19f0504ca3d1a14d0709baa250c
h4m41 <muhammedahmedcs@gmail.com>
2023-07-14 16:33:16 +0300

    Crud done

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const coonectDB = async() => {
+
+    try{
+const conn = await mongoose.connect(process.env.MONGO_URI)
+
+console.log(`MongoDB connected : ${conn.connection.host}`.cyan.underline )
+
+    }
+    catch(error){
+        console.log(error);
+        process.exit(1)
+    }
+}
+
+module.exports = coonectDB
--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,18 +1,49 @@
 const asyncHandler = require('express-async-handler')
 
+const Goal = require('../models/goalModel')
 
-const getGoals = asyncHandler (async (req,res) => {res.status(200).json({message: 'Get Goals'})})
+
+const getGoals = asyncHandler (async (req,res) => {
+    
+    const goals = await Goal.find()
+
+    res.status(200).json({ goals})})
 
 const createGoals =  asyncHandler (async  (req,res) => {
     
-    if(!req.body.text){res.status(400)
+    if (!req.body.text){res.status(400)
     throw new Error('please add a text field')}
+
+    const goal = await Goal.create({text: req.body.text})
     res.status(200).json({message: 'Create Goals'})}
     )
 
-const updateGoal= asyncHandler (async  (req,res) => {res.status(200).json({message:   `Update goal ${req.params.id}`})})
+const updateGoal= asyncHandler (async  (req,res) => {
+    
+    const goal = await Goal.findById(req.params.id); 
+    if (!goal){
+        res.status(400)
+        throw new Error('goal not found')
 
-const deleteGoal =asyncHandler ( async (req,res) => {res.status(200).json({message:   `Delete goal ${req.params.id}`})})
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body,{new: true})
+
+
+
+    res.status(200).json({updatedGo})})
+
+const deleteGoal =asyncHandler ( async (req,res) => {
+    
+    const goal = await Goal.findById(req.params.id); 
+    if (!goal){
+        res.status(400)
+        throw new Error('goal not found')
+    }
+
+  await goal.deleteOne()
+    
+    res.status(200).json({id: req.params.id})})
 
 
 
